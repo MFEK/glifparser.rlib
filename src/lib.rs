77356ed14225ad39e842ed40721c13355243e417
Matthew Blanchard <matthewblanc@msn.com>
2021-01-10 13:15:56 -0500

    Added an xml writer to glifparser.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(assoc_char_funcs, let_chains)]
 extern crate log;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum PointType {
     Undefined,
@@ -183,10 +184,14 @@ pub struct Glif<T> {
     pub format: u8, // we only understand 2
 }
 
+
 extern crate xmltree;
 use std::collections::VecDeque;
 use std::error::Error;
 use std::fs;
+
+extern crate xmlwriter;
+use xmlwriter::*;
 
 fn parse_anchor(anchor_el: xmltree::Element) -> Result<Anchor, &'static str> {
     Err("Unimplemented")
@@ -344,7 +349,7 @@ fn create_cubic_outline<T>(goutline: &GlifOutline) -> Outline<T> {
                 PointType::OffCurve => {
                     stack.push_back(&gp);
                 }
-                PointType::Line | PointType::Curve => {
+                PointType::Line | PointType::Move | PointType::Curve => {
                     let h1 = stack.pop_front();
                     let h2 = stack.pop_front();
 
@@ -376,10 +381,11 @@ fn create_cubic_outline<T>(goutline: &GlifOutline) -> Outline<T> {
             contour.first_mut().map(|p| p.b = Handle::from(h2));
         }
 
-        if contour.len() > 0 {
-            outline.push(contour);
-        } else {
+        if contour.len() == 1 && contour.first().unwrap().ptype == PointType::Move {
             log::warn!("Dropped empty contour. Lone `move` point in .glif? GlifContour: {:?}", &gc);
+        }
+        else if contour.len() > 0 {
+            outline.push(contour);
         }
     }
 
@@ -523,4 +529,137 @@ pub fn read_ufo_glif<T>(glif: &str) -> Glif<T> {
     }
 
     ret
+}
+
+fn point_type_to_string(ptype: PointType) -> Option<String>
+{
+    return match ptype{
+        PointType::Undefined => None,
+        PointType::OffCurve => None,
+        PointType::QClose => None, // should probably be removed from PointType
+        PointType::Move => Some(String::from("move")),
+        PointType::Curve => Some(String::from("curve")),
+        PointType::QCurve => Some(String::from("qcurve")),
+        PointType::Line => Some(String::from("line")),
+    }
+}
+
+fn write_ufo_point_from_handle(mut writer: XmlWriter, handle: Handle) -> XmlWriter
+{
+    match handle {
+        Handle::At(x, y) => {
+            writer.start_element("point");
+                writer.write_attribute("x", &x);
+                writer.write_attribute("y", &y);
+            writer.end_element();
+        },
+        _ => {}
+    }
+
+    return writer;
+}
+
+pub fn write_ufo_glif<T>(glif: Glif<T>) -> String
+{
+    let mut writer = XmlWriter::new(Options::default());
+
+    writer.start_element("glyph");
+    writer.write_attribute("name", &glif.name);
+    writer.write_attribute("format", &glif.format);
+
+    writer.start_element("advance");
+    writer.write_attribute("width", &glif.width);
+    writer.end_element();
+
+    match glif.unicode
+    {
+        Codepoint::Hex(hex) => {
+            writer.start_element("unicode");
+            writer.write_attribute("hex", &format!(r#"{:X}"#, hex as u32));
+            writer.end_element();
+        },
+        Codepoint::Undefined => {}
+    }
+
+    match glif.anchors
+    {
+        Some(anchor_vec) => {
+            for anchor in anchor_vec {
+                writer.start_element("anchor");
+                writer.write_attribute("x", &anchor.x);
+                writer.write_attribute("y", &anchor.y);
+                writer.write_attribute("name", &anchor.class);
+                // Anchor does not currently contain a color, or identifier attribute
+                writer.end_element();
+            }
+        },
+        None => {}
+    }
+
+    match glif.outline
+    {
+        Some(outline) => {
+            writer.start_element("outline");
+        
+            // if we find a move point at the start of things we set this to false
+
+            for contour in outline {
+                let open_contour = if contour.first().unwrap().ptype == PointType::Move { true } else { false };
+
+
+                writer.start_element("contour");
+                
+                let mut last_point = None;
+                for point in &contour {
+                    if let Some(lp) = last_point {
+                        // if there was a point prior to this one we emit our b handle
+                        writer = write_ufo_point_from_handle(writer, point.b);
+                    }
+
+
+                
+                    writer.start_element("point");
+                        writer.write_attribute("x", &point.x);
+                        writer.write_attribute("y", &point.y);
+                
+                        match point_type_to_string(point.ptype) {
+                            Some(ptype_string) => writer.write_attribute("type", &ptype_string),
+                            None => {}
+                        }
+                
+                        match &point.name {
+                            Some(name) => writer.write_attribute("name", &name),
+                            None => {}
+                        }
+                
+                        // Point>T> does not contain fields for smooth, or identifier.
+                    writer.end_element();
+                
+                    match point.ptype {
+                        PointType::Line | PointType::Curve => {
+                            writer = write_ufo_point_from_handle(writer, point.a);
+                        },
+
+                        PointType::QCurve => {
+                            //QCurve currently unhandled. This needs to be implemented.
+                        },
+                        _ => { } // I don't think this should be reachable in a well formed Glif object?
+                    }    
+                    
+                    last_point = Some(point);
+                }
+
+                // if a move wasn't our first point then we gotta close the shape by emitting the first point's b handle
+                if !open_contour {
+                    writer = write_ufo_point_from_handle(writer, contour.first().unwrap().b);
+                }
+
+                writer.end_element();
+            }
+            writer.end_element();
+        },
+        None => {}
+    }
+
+    writer.end_document()
 }
