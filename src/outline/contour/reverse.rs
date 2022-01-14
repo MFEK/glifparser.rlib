bd6f0b3e9d41700f90afe970473adfb93d11eb56
Fredrick Brennan <copypaste@kittens.ph>
2022-01-14 03:24:39 -0500

    Add contour reversal tests

--- src/outline/contour/reverse.rs
+++ src/outline/contour/reverse.rs
@@ -1,4 +1,5 @@
 use super::Contour;
+use crate::outline::refigure::RefigurePointTypes as _;
 use crate::point::{Handle, Point, PointData, PointType};
 
 // TODO: Implement this trait on the `data` members of ContourOperations.
@@ -6,7 +7,8 @@ use crate::point::{Handle, Point, PointData, PointType};
 /// necessary, and taking into account open/closed state.
 pub trait Reverse: Sized + Clone {
     fn to_reversed(&self) -> Self;
-    // Following semantics of std::slice::reverse
+    /// Following semantics of std::slice::reverse
+    /// Performance note: internally makes a copy. Beware overlong contours.
     fn reverse(&mut self) {
         let reversed = self.clone().to_reversed();
         *self = reversed;
@@ -36,23 +38,30 @@ impl<PD: PointData> Reverse for Contour<PD> {
             let b = p.b;
             new_p.a = b;
             new_p.b = a;
-            new_p.ptype = if a != Handle::Colocated {
-                PointType::Curve
-            } else {
-                PointType::Line
-            };
             new_c.push(new_p);
         }
 
+        let c_len = new_c.len();
         // Considering only open contours post-reversal, reverse which point is the Move.
         if open_contour {
             new_c[0].ptype = PointType::Move;
-            let c_len = new_c.len();
-            let ptype = match new_c[c_len - 2].a {
-                Handle::At(_, _) => PointType::Curve,
-                Handle::Colocated => PointType::Line,
+            let ptype = if c_len >= 3 {
+                match (new_c[c_len - 2].a, new_c[c_len - 1].b) {
+                    (Handle::At(..), _) | (_, Handle::At(..)) => PointType::Curve,
+                    (Handle::Colocated, Handle::Colocated) => PointType::Line,
+                }
+            } else if c_len == 2 {
+                match (new_c[0].a, new_c[1].b) {
+                    (Handle::At(..), _) | (_, Handle::At(..)) => PointType::Curve,
+                    (Handle::Colocated, Handle::Colocated) => PointType::Line,
+                }
+            } else {
+                log::error!("You probably should not be trying to reverse single-point contours. 気をつけてくださいね。");
+                return new_c
             };
             new_c[c_len - 1].ptype = ptype;
+            debug_assert!(new_c[0].b == Handle::Colocated);
+            debug_assert!(new_c[c_len - 1].a == Handle::Colocated);
         }
 
         new_c
---
+++ tests/reverse.rs
@@ -0,0 +1,42 @@
+use glifparser::outline::{Contour, Outline};
+use glifparser::outline::Reverse as _;
+use glifparser::outline::FromKurbo as _;
+use kurbo::Shape as _;
+
+#[macro_use] extern crate lazy_static;
+
+lazy_static! {
+    static ref CIRCLE: Outline::<()> = Outline::<()>::from_kurbo(&kurbo::BezPath::from_vec(kurbo::Circle::new(kurbo::Point::new(0.0, 0.0), 50.0).path_elements(f64::EPSILON).collect()));
+}
+static ITERATIONS: usize = 5_000;
+
+#[test]
+fn test_rev_outline() {
+    let gcircle = CIRCLE.clone();
+    let mut gcircle2 = CIRCLE.clone();
+    for i in 0 .. ITERATIONS {
+        gcircle2.reverse();
+    }
+    assert_eq!(gcircle, gcircle2);
+    let mut gcircle2 = CIRCLE.clone();
+    for i in 0 .. ITERATIONS - 1 {
+        gcircle2.reverse();
+    }
+    assert_ne!(gcircle, gcircle2);
+}
+
+#[test]
+fn test_rev_contour() {
+    let gcircle = &*CIRCLE;
+    let gcircle_contour: Contour<()> = gcircle.clone().pop().unwrap();
+    let mut gcircle2_contour: Contour<()> = gcircle_contour.clone();
+    for i in 0 .. ITERATIONS {
+        gcircle2_contour.reverse();
+    }
+    assert_eq!(gcircle_contour, gcircle2_contour);
+    let mut gcircle2_contour: Contour<()> = gcircle_contour.clone();
+    for i in 0 .. ITERATIONS - 1 {
+        gcircle2_contour.reverse();
+    }
+    assert_ne!(gcircle_contour, gcircle2_contour);
+}
