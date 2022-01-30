98ab5c6a30c85e159a79350b72f4b625a95a5820
Fredrick Brennan <copypaste@kittens.ph>
2022-01-30 01:23:08 -0500

    Add back AnchorType, now widely understood by ufo2ft
    _ as first char → mark
    else → base

--- src/anchor.rs
+++ src/anchor.rs
@@ -12,12 +12,45 @@ use crate::point::PointData;
 use crate::string::GlifStringLenOne;
 
 #[cfg_attr(feature = "glifserde", derive(Serialize, Deserialize))]
+#[derive(Clone, Debug, Derivative, PartialEq, Eq, Unwrap, IsVariant)]
+#[derivative(Default(new="true"))]
+pub enum AnchorType {
+    Mark,
+    #[derivative(Default)]
+    Base,
+}
+
+impl<S> From<S> for AnchorType where S: AsRef<str> {
+    fn from(s: S) -> Self {
+        if s.as_ref().chars().nth(0) == Some('_') {
+            Self::Mark
+        } else {
+            Self::Base
+        }
+    }
+}
+
+pub trait FromOption<S: Into<AnchorType>>: Default {
+    fn from_option(s: Option<S>) -> AnchorType {
+        match s {
+            Some(ss) => ss.into(),
+            None => AnchorType::default()
+        }
+    }
+}
+impl<S: Into<Self>> FromOption<S> for AnchorType {}
+
+#[cfg_attr(feature = "glifserde", derive(Serialize, Deserialize))]
 #[derive(Constructor, Clone, Debug, Default, PartialEq)]
 pub struct Anchor<PD: PointData> {
     pub x: f32,
     pub y: f32,
+    #[cfg_attr(feature = "glifserde", serde(default))]
     pub class: Option<String>,
+    #[cfg_attr(feature = "glifserde", serde(default))]
     pub data: PD,
+    #[cfg_attr(feature = "glifserde", serde(default))]
+    pub atype: AnchorType,
 }
 
 impl<PD: PointData> Anchor<PD> {
@@ -27,8 +60,9 @@ impl<PD: PointData> Anchor<PD> {
         if pedantry.mend != Mend::Always && (!ga.x.holding_integer().is_ok() || !ga.y.holding_integer().is_ok()) {
             return Err(GlifParserError::PedanticXmlParseError("Anchor was a float, not an integer!".to_string()))
         }
+        let atype = AnchorType::from_option(class.as_ref());
         Ok(Self {
-            x, y, class, data: PD::default()
+            x, y, class, atype, data: PD::default()
         })
     }
 }
